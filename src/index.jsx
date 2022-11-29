ce3998209f0d5ad843b75bf32030a0936d05f9a3
Juan Bezanilla <juanbezanillaz@gmail.com>
2022-11-29 19:03:04 -0300

    props

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
 
-const Hello = (props) => {
-  return <div>Hello, {props.name} </div>;
+// const Hello = (props) => {
+//   return <div>Hello, {props.name} </div>;
+// }
+
+class Hello extends Component {
+  constructor (props) {
+    super (props);
+
+    this.state = {
+      clicked: false
+    };
+  }
+
+  render () {
+    // BUILD AND RETURN HTML
+    return (
+      <div className={this.state.clicked ? 'cliked' : null}>
+        Hello {this.props.name}
+      </div>
+    )
+  }
 }
 
 const root = document.getElementById('root');
