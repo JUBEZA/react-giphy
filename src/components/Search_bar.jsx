a80ca9d9c94af85717e5bee6a4cccb1413e22dbd
Juan Bezanilla <juanbezanillaz@gmail.com>
2022-12-01 22:36:43 -0300

    antes de arreglar search bar

--- src/components/Search_bar.jsx
+++ src/components/Search_bar.jsx
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  handleUpdate = (event) => {
+    console.log(event.target.value);
+  }
+
   render( ) {
     return (
-      <input type="text" className="form-control formsearch" />
+      <input type="text" className="form-control formsearch"
+        onChange={this.handleUpdate}/>
     );
   }
 }
--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
+import giphy from 'ghiphy-api';
 
 import SearchBar from './search_bar.jsx';
 import Gif from './gif.jsx'
 import GifList from './gif_list.jsx';
 
 class App extends Component {
+  constructor(props) {
+    super (props);
+
+    this.state = {
+      gifs: [],
+      selectedGifId: "xT9IgDEI1iZyb2wqo8"
+    }
+
+    this.search("homer thinking");
+  }
+
+  search() = (query) => {
+    giphy('1KMPHCBIOe3hOjJwCJQX49sRc6cMOoiM').search({
+      q: query,
+      rating: 'g'
+      limit: 10
+    }, function (error, result) => {
+      this.setState({
+        gifs: result.data
+      })
+    })
+  }
+
   render () {
     const gifs = [
       { id: "xT9IgDEI1iZyb2wqo8" },
@@ -19,7 +43,7 @@ class App extends Component {
         </div>
       </div>
       <div className="right-scene"> </div>
-        <Giflist gifs={gifs}/>
+        <Giflist gifs={this.state.gifs}/>
     </div>
    );
   }
